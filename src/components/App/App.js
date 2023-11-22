3063d92aa820446004b9b161541a6ce785e9866d
Dmytro Korolik <dimytry.korolik@gmail.com>
2023-11-23 01:01:12 +0200

    api-fn-done

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,5 +1,11 @@
 import React from 'react';
 import { NavLink, Route, Routes } from 'react-router-dom';
+import { Home } from 'pages';
+import {
+  getMovieCredits,
+  getMovieDetails,
+  getTrendingMovie,
+} from 'helpers/api';
 
 export const App = () => {
   return (
@@ -23,15 +29,20 @@ export const App = () => {
             </div>
           }
         />
-        <Route
-          path="/movies"
-          element={
-            <div>
-              <h1>Movies</h1>
-            </div>
-          }
-        />
+        <Route path="/movies" element={<Home />} />
       </Routes>
     </>
   );
 };
+
+const trendingMovies = async () => {
+  try {
+    const response = await getMovieCredits(555);
+
+    console.log(response);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+trendingMovies();
---
+++ src/helpers/api.js
@@ -0,0 +1,39 @@
+import axios from 'axios';
+
+axios.defaults.baseURL = `https://api.themoviedb.org/3`;
+axios.defaults.headers.common['Authorization'] =
+  'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1ZDQ0NWFiZGM2Njk2ZGJkZmU1MmU4ZTY5YWI5NzcyNCIsInN1YiI6IjY1NGY2YjExMjg2NmZhMTA4YjZlMGQ5MyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.cMDqHvE6cKl3jitWT16Pyhx2laItjGSAzGHFfRL7D3U';
+
+// /trending/get-trending список найпопулярніших фільмів на сьогодні для створення колекції на головній сторінці.
+export const getTrendingMovie = async () => {
+  const response = await axios.get(`/trending/movie/day?language=en-US`);
+  return response.data;
+};
+
+// /search/search-movies пошук фільму за ключовим словом на сторінці фільмів.
+export const searchMovies = async query => {
+  const response = await axios.get(
+    `/search/movie?query=${query}&include_adult=false&language=en-US&page=1`
+  );
+  return response.data;
+};
+
+// /movies/get-movie-details запит повної інформації про фільм для сторінки кінофільму.
+export const getMovieDetails = async id => {
+  const response = await axios.get(`/movie/${id}?language=en-US`);
+  return response.data;
+};
+
+// /movies/get-movie-credits запит інформації про акторський склад для сторінки кінофільму.
+export const getMovieCredits = async id => {
+  const response = await axios.get(`/movie/${id}/credits?language=en-US`);
+  return response.data;
+};
+
+// /movies/get-movie-reviews запит оглядів для сторінки кінофільму.
+export const getMovieReviews = async id => {
+  const response = await axios.get(
+    `/movie/${id}/reviews?language=en-US&page=1`
+  );
+  return response.data;
+};
